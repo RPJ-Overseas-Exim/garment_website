fe7eb7661f43b636efe514eea55dbec39ae17439
Vikrant <tiwarivikrant9764@gmail.com>
2024-07-17 17:55:40 +0530

    New arrival section completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/libs/components/HeroSection/HeroSection";
+import NewArrival from "@/libs/components/NewArrival/NewArrival";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
+      <NewArrival />
     </main>
   );
 }
--- libs/components/Navbar/Navbar.tsx
+++ libs/components/Navbar/Navbar.tsx
@@ -9,7 +9,7 @@ import { IoMdClose } from "react-icons/io";
 import { useState } from "react";
 
 export default function Navbar() {
-  const [userLoggedIn, setUserLoggedIn] = useState(true);
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   function openMenu() {
