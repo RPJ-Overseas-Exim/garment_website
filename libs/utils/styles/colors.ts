66098f02a8452fbc446b413e4cdb99fa48631508
muzammil <saifimuzammil2@gmail.com>
2024-07-24 17:12:51 +0530

    minor refactor

--- libs/utils/styles/colors.ts
+++ libs/utils/styles/colors.ts
@@ -1,8 +1,8 @@
-const colorsObject = {
+const dashColors = {
   deepBlue: "#0095FF",
   lightGreen: "#00E096",
   lavender: "#884DFF",
   lightOrange: "#FF8F0D",
 };
 
-export default colorsObject;
+export default dashColors;
