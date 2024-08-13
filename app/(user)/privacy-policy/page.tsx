f24ec130c353a050082b38f90628634aec17511a
Vikrant <tiwarivikrant9764@gmail.com>
2024-08-13 11:24:01 +0530

    profile fix and policy pages minor fixes

--- app/(user)/privacy-policy/page.tsx
+++ app/(user)/privacy-policy/page.tsx
@@ -7,67 +7,68 @@ export default function Privacy() {
         </h1>
         <hr className="w-full border-[var(--text-regular)]" />
         <h2 className="text-[var(--text-regular)] font-[400] text-md mt-2 mb-3">
-          Last updated April 31, 2024
+          {`Last updated April 31, 2024`}
         </h2>
         <div className="mt-3 mb-3 text-[var(--text-regular)] font-md text-lg flex flex-col gap-y-2">
           <p>
-            <b>YourClothingStore</b> is committed to your privacy and will not
+            <b>YourClothingStore</b>
+            {` is committed to your privacy and will not
             share any user information with any third party without your
             consent, except as outlined in the privacy statement or as permitted
             by law. If you have any comments, requests, or suggestions about the
             privacy statement or user information, please contact our support
-            team.
+            team.`}
           </p>
           <p>
-            To enhance, customize, and support your experience on our website,
+            {`To enhance, customize, and support your experience on our website,
             we collect, utilize, and store your information. Your consent to the
             collection, use, and storage of your data is outlined in this
             privacy statement by accessing or using this website. Please read
             this policy carefully before using the site or submitting personal
             information to us. This policy statement is incorporated and subject
-            to the terms and conditions.
+            to the terms and conditions.`}
           </p>
         </div>
         <div className="mt-3 mb-3 font-[600] text-[var(--text-regular)] text-lg">
           <details>
             <summary className="font-semibold text-xl text-[var(--text-dark)]">
-              What This Privacy Statement Covers?
+              {"What This Privacy Statement Covers?"}
             </summary>
             <p className="mt-2 mb-2 pl-7">
-              This privacy statement covers the website information. While you
+              {`This privacy statement covers the website information. While you
               may encounter links from our website to other websites, such as
               those relating to third parties or advertisers, please note that
               our privacy policy does not cover those external websites. The
               privacy and information practices of third parties are not covered
               in this statement. This policy statement applies only to
-              information collected by us.
+              information collected by us.`}
             </p>
           </details>
         </div>
         <div className="mt-3 mb-3 font-[600] text-[var(--text-regular)] text-lg">
           <details>
             <summary className="font-semibold text-xl text-[var(--text-dark)]">
-              What Personal Information Does YourClothingStore Gather?
+              {`What Personal Information Does YourClothingStore Gather?`}
             </summary>
             <p className="mt-2 mb-2 pl-7">
-              YourClothingStore only gathers essential details such as your
+              {`YourClothingStore only gathers essential details such as your
               name, contact number, email, and previous orders. None of your
               financial or bank details will be stored on any of our servers or
               website. We collect information when you use our website.
               Sometimes you may submit it to yourself via emails or product
               inquiries; other times, it may be collected automatically. You may
               manage and change your privacy settings according to your
-              preferences.
+              preferences.`}
             </p>
           </details>
         </div>
         <div className="mt-3 mb-3 font-[600] text-[var(--text-regular)] text-lg">
           <details>
             <summary className="font-semibold text-xl text-[var(--text-dark)]">
-              How Secure Is Your Information With YourClothingStore?
+              {`How Secure Is Your Information With YourClothingStore?`}
             </summary>
             <p className="mt-2 mb-2 pl-7">
-              YourClothingStore does not sell, share, or distribute your data
+              {`YourClothingStore does not sell, share, or distribute your data
               with third parties for any marketing purposes. We will only share
               your data in compliance with applicable laws and regulations.
               YourClothingStore has implemented a data security program
@@ -81,42 +82,42 @@ export default function Privacy() {
               the International Standard IS/ISO/IEC 27001 on "Information
               Technology Security Techniques Information Security Management
               System-Requirements." Our IT team works around the clock to
-              prevent any issues and ensure your peace of mind.
+              prevent any issues and ensure your peace of mind.`}
             </p>
           </details>
         </div>
         <div className="mt-3 mb-3 font-[600] text-[var(--text-regular)] text-lg">
           <details>
             <summary className="font-semibold text-xl text-[var(--text-dark)]">
-              Does YourClothingStore Disclose The Information It Receives?
+              {`Does YourClothingStore Disclose The Information It Receives?`}
             </summary>
             <p className="mt-2 mb-2 pl-7">
-              We guarantee that no personal information you provide will be used
+              {`We guarantee that no personal information you provide will be used
               or shared in any way that is incompatible with this Privacy
               Statement without your permission. Only our delivery employees
-              will have access to the information you require.
+              will have access to the information you require.`}
             </p>
           </details>
         </div>
         <div className="mt-3 mb-3 font-[600] text-[var(--text-regular)] text-lg">
           <details>
             <summary className="font-semibold text-xl text-[var(--text-dark)]">
-              Children's Privacy
+              {`Children's Privacy`}
             </summary>
             <p className="mt-2 mb-2 pl-7">
-              YourClothingStore websites are not intended to attract children,
+              {`YourClothingStore websites are not intended to attract children,
               and we do not intentionally collect any personal data from anyone
-              under the age of 18.
+              under the age of 18.`}
             </p>
           </details>
         </div>
         <div className="mt-3 mb-3 font-[600] text-[var(--text-regular)] text-lg">
           <details>
             <summary className="font-semibold text-xl text-[var(--text-dark)]">
-              Forums, Product Reviews, and Other Areas
+              {`Forums, Product Reviews, and Other Areas`}
             </summary>
             <p className="mt-2 mb-2 pl-7">
-              Our websites may include forums and other areas where you can
+              {`Our websites may include forums and other areas where you can
               publish reviews, chat, and submit content to the public. Please
               read our terms of service carefully before submitting anything.
               Public postings do not have any privacy protections. Anyone with
@@ -124,19 +125,19 @@ export default function Privacy() {
               and any personal data you post can be read, collected, and used by
               others. If you post a product review with your email address, for
               example, you may receive unsolicited messages. When posting any
-              personal information, please use caution.
+              personal information, please use caution.`}
             </p>
           </details>
         </div>
         <div className="mt-3 mb-3 font-[600] text-[var(--text-regular)] text-lg">
           <details>
             <summary className="font-semibold text-xl text-[var(--text-dark)]">
-              Changes To This Privacy Statement
+              {`Changes To This Privacy Statement`}
             </summary>
             <p className="mt-2 mb-2 pl-7">
-              We reserve the right to revise the terms of the privacy statement
+              {`We reserve the right to revise the terms of the privacy statement
               at any time on our website. If we make changes to the privacy
-              statement, we will notify you with a prominent notice via email.
+              statement, we will notify you with a prominent notice via email.`}
             </p>
           </details>
         </div>
@@ -146,13 +147,13 @@ export default function Privacy() {
               Cookies
             </summary>
             <p className="mt-2 mb-2 pl-7">
-              A cookie is a text file generated by a website, such as a password
+              {`A cookie is a text file generated by a website, such as a password
               or login, that is saved in your web browser by default. Its
               purpose is to save your information, similar to a preference file
               created by a software application based on your interactions. To
               optimize users' web browsing experience, certain cookies are
               utilized to identify them. We do not, however, use cookies to save
-              any information other than what you choose to provide us.
+              any information other than what you choose to provide us.`}
             </p>
           </details>
         </div>
@@ -161,8 +162,8 @@ export default function Privacy() {
             Your Clothes Store
           </div>
           <p className="mt-2 mb-2 pl-7 flex flex-col ">
-            <span>Phone: +1 713 893 7357 </span>
-            <span>benzosupplies.usatousa@gmail.com</span>
+            <span>{`Phone: +1 number `}</span>
+            <span>{`abc@gmail.com`}</span>
           </p>
         </div>
       </div>
--- app/(user)/shipping-policy/page.tsx
+++ app/(user)/shipping-policy/page.tsx
@@ -9,30 +9,33 @@ export default function page() {
         </h1>
         <hr className="w-full border-[var(--text-regular)]" />
         <h2 className="text-[var(--text-regular)] font-[400] text-md mt-2 mb-3">
-          Last updated April 31, 2024
+          {`Last updated April 31, 2024`}
         </h2>
 
         <div className="mt-3 mb-3 font-[600] text-[var(--text-regular)] text-lg">
-          <b>Your Clothes Store</b> is offering their customers two different
+          <b>Your Clothes Store</b>
+          {` is offering their customers two different
           delivery options to choose from. Customers can select between any type
           of shipping according to their needs, i.e., Global Shipping & USA
-          Premium Shipping.
+          Premium Shipping.`}
         </div>
         <div className="mt-3 mb-3 font-[600] text-[var(--text-regular)] text-lg">
           <ul className="flex flex-col gap-y-3 list-decimal pl-7">
             <li>
-              <b>Global Shipping: </b> With Global Shipping, clothes are shipped
+              <b>{`Global Shipping: `}</b>{" "}
+              {`With Global Shipping, clothes are shipped
               all over the globe. Delivery comes with nominal charges. Global
               Shipping typically takes 12 to 18 business days to deliver. Except
               for seven specific clothes from USA Premium, all other clothes
-              mentioned on the website are shipped globally.
+              mentioned on the website are shipped globally.`}
             </li>
             <li>
-              <b>USA Premium Shipping: </b> In USA Premium Shipping, clothes are
+              <b>{`USA Premium Shipping: `}</b>{" "}
+              {`In USA Premium Shipping, clothes are
               shipped from storage facilities located in the USA, which makes
               delivery time surprisingly short. There are no delivery charges
               for USA Premium Shipping, and it takes 5 to 7 business days to
-              deliver.
+              deliver.`}
             </li>
           </ul>
         </div>
@@ -41,8 +44,8 @@ export default function page() {
             Your Clothes Store
           </div>
           <p className="mt-2 mb-2 pl-7 flex flex-col">
-            <span>Phone: +1 713 893 7357 </span>
-            <span>benzosupplies.usatousa@gmail.com</span>
+            <span>{`Phone: +1 number `}</span>
+            <span>{`abc@gmail.com`}</span>
           </p>
         </div>
       </div>
