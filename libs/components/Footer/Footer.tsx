d37ed41e18f63e3afa9608304eaa02bbbb4d4919
vikrant <tiwarivikrant9764@gmail.com>
2025-04-10 21:10:31 +0530

    changed the company name

--- libs/components/Footer/Footer.tsx
+++ libs/components/Footer/Footer.tsx
@@ -4,67 +4,67 @@ import { FaFacebook, FaEnvelope } from "react-icons/fa";
 import Link from "next/link";
 
 export default function Footer() {
-  return (
-    <footer className="footer">
-      <div className="footer__container container">
-        <h2 className="footer__title">RPJ Garments</h2>
+    return (
+        <footer className="footer">
+            <div className="footer__container container">
+                <h2 className="footer__title">Myus Enterprises</h2>
 
-        <hr className="divider" />
+                <hr className="divider" />
 
-        <div className="footer__nav">
-          <div>
-            <h4 id="Terms-and-Policy">Terms and Policy</h4>
-            <ul aria-labelledby="Terms-and-Policy">
-              <li>
-                <Link href={"/returns"}>Return & Refund</Link>
-              </li>
-              <li>
-                <Link href={"/shipping-policy"}>Shipping Policy</Link>
-              </li>
-              <li>
-                <Link href={"/terms-and-condition"}>Terms and Conditions</Link>
-              </li>
-              <li>
-                <Link href={"/privacy-policy"}>Privacy Policy</Link>
-              </li>
-            </ul>
-          </div>
+                <div className="footer__nav">
+                    <div>
+                        <h4 id="Terms-and-Policy">Terms and Policy</h4>
+                        <ul aria-labelledby="Terms-and-Policy">
+                            <li>
+                                <Link href={"/returns"}>Return & Refund</Link>
+                            </li>
+                            <li>
+                                <Link href={"/shipping-policy"}>Shipping Policy</Link>
+                            </li>
+                            <li>
+                                <Link href={"/terms-and-condition"}>Terms and Conditions</Link>
+                            </li>
+                            <li>
+                                <Link href={"/privacy-policy"}>Privacy Policy</Link>
+                            </li>
+                        </ul>
+                    </div>
 
-          <div>
-            <h4 id="Location">Location</h4>
-            <ul aria-labelledby="Location">
-              <li>Mumbai-400001, Maharashtra</li>
-            </ul>
-          </div>
+                    <div>
+                        <h4 id="Location">Location</h4>
+                        <ul aria-labelledby="Location">
+                            <li>Mumbai-400001, Maharashtra</li>
+                        </ul>
+                    </div>
 
-          <div>
-            <h4 id="Contact">Contact</h4>
-            <ul aria-labelledby="Contact" className="footer__contact">
-              <li>
-                <Link href="/">
-                  <FaInstagram />
-                </Link>
-              </li>
-              <li>
-                <Link href="/">
-                  <FaFacebook />
-                </Link>
-              </li>
-              <li>
-                <Link href="/">
-                  <FaEnvelope />
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
+                    <div>
+                        <h4 id="Contact">Contact</h4>
+                        <ul aria-labelledby="Contact" className="footer__contact">
+                            <li>
+                                <Link href="/">
+                                    <FaInstagram />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/">
+                                    <FaFacebook />
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/">
+                                    <FaEnvelope />
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
 
-        <hr className="divider" />
+                <hr className="divider" />
 
-        <span className="footer__bottom">
-          Copyright &copy; RPJ Garments. All rights reserved.
-        </span>
-      </div>
-    </footer>
-  );
+                <span className="footer__bottom">
+                    Copyright &copy; Myus Enterprises. All rights reserved.
+                </span>
+            </div>
+        </footer>
+    );
 }
